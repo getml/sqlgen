e346d9bc2b92ca846a58feda9eb8790d68ebe96a
Dr. Patrick Urbanke <patrick@getml.com>
2025-05-20 20:13:52 +0200

    Began developing a separate insert command

--- include/sqlgen/sqlite/Connection.hpp
+++ include/sqlgen/sqlite/Connection.hpp
@@ -40,6 +40,11 @@ class Connection : public sqlgen::Connection {
 
   Result<Nothing> execute(const std::string& _sql) noexcept final;
 
+  Result<Nothing> insert(
+      const dynamic::Insert& _stmt,
+      const std::vector<std::vector<std::optional<std::string>>>&
+          _data) noexcept;
+
   Connection& operator=(const Connection& _other) = delete;
 
   Connection& operator=(Connection&& _other) noexcept;
@@ -62,6 +67,15 @@ class Connection : public sqlgen::Connection {
  private:
   /// Generates the underlying connection.
   static ConnPtr make_conn(const std::string& _fname);
+
+  /// Actually inserts data based on a prepared statement -
+  /// used by both .insert(...) and .write(...).
+  Result<Nothing> actual_insert(
+      const std::vector<std::vector<std::optional<std::string>>>& _data,
+      sqlite3_stmt* _stmt) const noexcept;
+
+  /// Generates a prepared statment, usually for inserts.
+  Result<StmtPtr> prepare_statement(const std::string& _sql) const noexcept;
 
  private:
   /// A prepared statement - needed for the read and write operations. Note that
