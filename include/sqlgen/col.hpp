1e7e87cdbf459df463ee1caa361e23fe871a102d
Dr. Patrick Urbanke <patrick@getml.com>
2025-07-14 22:32:46 +0200

    Use a macro for the alias

--- include/sqlgen/col.hpp
+++ include/sqlgen/col.hpp
@@ -230,500 +230,111 @@ auto operator"" _c() {
   return Col<_name>{};
 }
 
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t1() {
-  return Col<_name, "t1">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t2() {
-  return Col<_name, "t2">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t3() {
-  return Col<_name, "t3">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t4() {
-  return Col<_name, "t4">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t5() {
-  return Col<_name, "t5">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t6() {
-  return Col<_name, "t6">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t7() {
-  return Col<_name, "t7">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t8() {
-  return Col<_name, "t8">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t9() {
-  return Col<_name, "t9">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t10() {
-  return Col<_name, "t10">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t11() {
-  return Col<_name, "t11">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t12() {
-  return Col<_name, "t12">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t13() {
-  return Col<_name, "t13">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t14() {
-  return Col<_name, "t14">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t15() {
-  return Col<_name, "t15">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t16() {
-  return Col<_name, "t16">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t17() {
-  return Col<_name, "t17">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t18() {
-  return Col<_name, "t18">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t19() {
-  return Col<_name, "t19">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t20() {
-  return Col<_name, "t20">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t21() {
-  return Col<_name, "t21">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t22() {
-  return Col<_name, "t22">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t23() {
-  return Col<_name, "t23">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t24() {
-  return Col<_name, "t24">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t25() {
-  return Col<_name, "t25">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t26() {
-  return Col<_name, "t26">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t27() {
-  return Col<_name, "t27">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t28() {
-  return Col<_name, "t28">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t29() {
-  return Col<_name, "t29">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t30() {
-  return Col<_name, "t30">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t31() {
-  return Col<_name, "t31">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t32() {
-  return Col<_name, "t32">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t33() {
-  return Col<_name, "t33">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t34() {
-  return Col<_name, "t34">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t35() {
-  return Col<_name, "t35">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t36() {
-  return Col<_name, "t36">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t37() {
-  return Col<_name, "t37">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t38() {
-  return Col<_name, "t38">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t39() {
-  return Col<_name, "t39">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t40() {
-  return Col<_name, "t40">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t41() {
-  return Col<_name, "t41">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t42() {
-  return Col<_name, "t42">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t43() {
-  return Col<_name, "t43">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t44() {
-  return Col<_name, "t44">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t45() {
-  return Col<_name, "t45">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t46() {
-  return Col<_name, "t46">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t47() {
-  return Col<_name, "t47">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t48() {
-  return Col<_name, "t48">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t49() {
-  return Col<_name, "t49">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t50() {
-  return Col<_name, "t50">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t51() {
-  return Col<_name, "t51">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t52() {
-  return Col<_name, "t52">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t53() {
-  return Col<_name, "t53">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t54() {
-  return Col<_name, "t54">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t55() {
-  return Col<_name, "t55">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t56() {
-  return Col<_name, "t56">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t57() {
-  return Col<_name, "t57">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t58() {
-  return Col<_name, "t58">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t59() {
-  return Col<_name, "t59">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t60() {
-  return Col<_name, "t60">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t61() {
-  return Col<_name, "t61">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t62() {
-  return Col<_name, "t62">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t63() {
-  return Col<_name, "t63">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t64() {
-  return Col<_name, "t64">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t65() {
-  return Col<_name, "t65">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t66() {
-  return Col<_name, "t66">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t67() {
-  return Col<_name, "t67">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t68() {
-  return Col<_name, "t68">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t69() {
-  return Col<_name, "t69">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t70() {
-  return Col<_name, "t70">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t71() {
-  return Col<_name, "t71">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t72() {
-  return Col<_name, "t72">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t73() {
-  return Col<_name, "t73">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t74() {
-  return Col<_name, "t74">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t75() {
-  return Col<_name, "t75">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t76() {
-  return Col<_name, "t76">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t77() {
-  return Col<_name, "t77">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t78() {
-  return Col<_name, "t78">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t79() {
-  return Col<_name, "t79">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t80() {
-  return Col<_name, "t80">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t81() {
-  return Col<_name, "t81">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t82() {
-  return Col<_name, "t82">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t83() {
-  return Col<_name, "t83">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t84() {
-  return Col<_name, "t84">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t85() {
-  return Col<_name, "t85">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t86() {
-  return Col<_name, "t86">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t87() {
-  return Col<_name, "t87">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t88() {
-  return Col<_name, "t88">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t89() {
-  return Col<_name, "t89">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t90() {
-  return Col<_name, "t90">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t91() {
-  return Col<_name, "t91">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t92() {
-  return Col<_name, "t92">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t93() {
-  return Col<_name, "t93">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t94() {
-  return Col<_name, "t94">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t95() {
-  return Col<_name, "t95">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t96() {
-  return Col<_name, "t96">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t97() {
-  return Col<_name, "t97">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t98() {
-  return Col<_name, "t98">{};
-}
-
-template <rfl::internal::StringLiteral _name>
-auto operator"" _t99() {
-  return Col<_name, "t99">{};
-}
+#define SQLGEN_TABLE_ALIAS_LITERAL(N)           \
+  template <rfl::internal::StringLiteral _name> \
+  auto operator""_t##N() {                      \
+    return Col<_name, "t" #N>{};                \
+  }
+
+SQLGEN_TABLE_ALIAS_LITERAL(1)
+SQLGEN_TABLE_ALIAS_LITERAL(2)
+SQLGEN_TABLE_ALIAS_LITERAL(3)
+SQLGEN_TABLE_ALIAS_LITERAL(4)
+SQLGEN_TABLE_ALIAS_LITERAL(5)
+SQLGEN_TABLE_ALIAS_LITERAL(6)
+SQLGEN_TABLE_ALIAS_LITERAL(7)
+SQLGEN_TABLE_ALIAS_LITERAL(8)
+SQLGEN_TABLE_ALIAS_LITERAL(9)
+SQLGEN_TABLE_ALIAS_LITERAL(10)
+SQLGEN_TABLE_ALIAS_LITERAL(11)
+SQLGEN_TABLE_ALIAS_LITERAL(12)
+SQLGEN_TABLE_ALIAS_LITERAL(13)
+SQLGEN_TABLE_ALIAS_LITERAL(14)
+SQLGEN_TABLE_ALIAS_LITERAL(15)
+SQLGEN_TABLE_ALIAS_LITERAL(16)
+SQLGEN_TABLE_ALIAS_LITERAL(17)
+SQLGEN_TABLE_ALIAS_LITERAL(18)
+SQLGEN_TABLE_ALIAS_LITERAL(19)
+SQLGEN_TABLE_ALIAS_LITERAL(20)
+SQLGEN_TABLE_ALIAS_LITERAL(21)
+SQLGEN_TABLE_ALIAS_LITERAL(22)
+SQLGEN_TABLE_ALIAS_LITERAL(23)
+SQLGEN_TABLE_ALIAS_LITERAL(24)
+SQLGEN_TABLE_ALIAS_LITERAL(25)
+SQLGEN_TABLE_ALIAS_LITERAL(26)
+SQLGEN_TABLE_ALIAS_LITERAL(27)
+SQLGEN_TABLE_ALIAS_LITERAL(28)
+SQLGEN_TABLE_ALIAS_LITERAL(29)
+SQLGEN_TABLE_ALIAS_LITERAL(30)
+SQLGEN_TABLE_ALIAS_LITERAL(31)
+SQLGEN_TABLE_ALIAS_LITERAL(32)
+SQLGEN_TABLE_ALIAS_LITERAL(33)
+SQLGEN_TABLE_ALIAS_LITERAL(34)
+SQLGEN_TABLE_ALIAS_LITERAL(35)
+SQLGEN_TABLE_ALIAS_LITERAL(36)
+SQLGEN_TABLE_ALIAS_LITERAL(37)
+SQLGEN_TABLE_ALIAS_LITERAL(38)
+SQLGEN_TABLE_ALIAS_LITERAL(39)
+SQLGEN_TABLE_ALIAS_LITERAL(40)
+SQLGEN_TABLE_ALIAS_LITERAL(41)
+SQLGEN_TABLE_ALIAS_LITERAL(42)
+SQLGEN_TABLE_ALIAS_LITERAL(43)
+SQLGEN_TABLE_ALIAS_LITERAL(44)
+SQLGEN_TABLE_ALIAS_LITERAL(45)
+SQLGEN_TABLE_ALIAS_LITERAL(46)
+SQLGEN_TABLE_ALIAS_LITERAL(47)
+SQLGEN_TABLE_ALIAS_LITERAL(48)
+SQLGEN_TABLE_ALIAS_LITERAL(49)
+SQLGEN_TABLE_ALIAS_LITERAL(50)
+SQLGEN_TABLE_ALIAS_LITERAL(51)
+SQLGEN_TABLE_ALIAS_LITERAL(52)
+SQLGEN_TABLE_ALIAS_LITERAL(53)
+SQLGEN_TABLE_ALIAS_LITERAL(54)
+SQLGEN_TABLE_ALIAS_LITERAL(55)
+SQLGEN_TABLE_ALIAS_LITERAL(56)
+SQLGEN_TABLE_ALIAS_LITERAL(57)
+SQLGEN_TABLE_ALIAS_LITERAL(58)
+SQLGEN_TABLE_ALIAS_LITERAL(59)
+SQLGEN_TABLE_ALIAS_LITERAL(60)
+SQLGEN_TABLE_ALIAS_LITERAL(61)
+SQLGEN_TABLE_ALIAS_LITERAL(62)
+SQLGEN_TABLE_ALIAS_LITERAL(63)
+SQLGEN_TABLE_ALIAS_LITERAL(64)
+SQLGEN_TABLE_ALIAS_LITERAL(65)
+SQLGEN_TABLE_ALIAS_LITERAL(66)
+SQLGEN_TABLE_ALIAS_LITERAL(67)
+SQLGEN_TABLE_ALIAS_LITERAL(68)
+SQLGEN_TABLE_ALIAS_LITERAL(69)
+SQLGEN_TABLE_ALIAS_LITERAL(70)
+SQLGEN_TABLE_ALIAS_LITERAL(71)
+SQLGEN_TABLE_ALIAS_LITERAL(72)
+SQLGEN_TABLE_ALIAS_LITERAL(73)
+SQLGEN_TABLE_ALIAS_LITERAL(74)
+SQLGEN_TABLE_ALIAS_LITERAL(75)
+SQLGEN_TABLE_ALIAS_LITERAL(76)
+SQLGEN_TABLE_ALIAS_LITERAL(77)
+SQLGEN_TABLE_ALIAS_LITERAL(78)
+SQLGEN_TABLE_ALIAS_LITERAL(79)
+SQLGEN_TABLE_ALIAS_LITERAL(80)
+SQLGEN_TABLE_ALIAS_LITERAL(81)
+SQLGEN_TABLE_ALIAS_LITERAL(82)
+SQLGEN_TABLE_ALIAS_LITERAL(83)
+SQLGEN_TABLE_ALIAS_LITERAL(84)
+SQLGEN_TABLE_ALIAS_LITERAL(85)
+SQLGEN_TABLE_ALIAS_LITERAL(86)
+SQLGEN_TABLE_ALIAS_LITERAL(87)
+SQLGEN_TABLE_ALIAS_LITERAL(88)
+SQLGEN_TABLE_ALIAS_LITERAL(89)
+SQLGEN_TABLE_ALIAS_LITERAL(90)
+SQLGEN_TABLE_ALIAS_LITERAL(91)
+SQLGEN_TABLE_ALIAS_LITERAL(92)
+SQLGEN_TABLE_ALIAS_LITERAL(93)
+SQLGEN_TABLE_ALIAS_LITERAL(94)
+SQLGEN_TABLE_ALIAS_LITERAL(95)
+SQLGEN_TABLE_ALIAS_LITERAL(96)
+SQLGEN_TABLE_ALIAS_LITERAL(97)
+SQLGEN_TABLE_ALIAS_LITERAL(98)
+SQLGEN_TABLE_ALIAS_LITERAL(99)
 
 }  // namespace sqlgen
 
