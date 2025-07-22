3943aeb7d6d588681541711468aa38c382b8a18b
Dr. Patrick Urbanke <patrick@getml.com>
2025-07-22 20:10:02 +0200

    Added the mysql::Iterator

--- include/sqlgen/mysql/Iterator.hpp
+++ include/sqlgen/mysql/Iterator.hpp
@@ -1,7 +1,7 @@
-#ifndef SQLGEN_POSTGRES_ITERATOR_HPP_
-#define SQLGEN_POSTGRES_ITERATOR_HPP_
+#ifndef SQLGEN_MYSQL_ITERATOR_HPP_
+#define SQLGEN_MYSQL_ITERATOR_HPP_
 
-#include <libpq-fe.h>
+#include <mysql.h>
 
 #include <memory>
 #include <optional>
@@ -13,22 +13,19 @@
 #include "../Result.hpp"
 #include "Connection.hpp"
 
-namespace sqlgen::postgres {
+namespace sqlgen::mysql {
 
 class Iterator : public sqlgen::IteratorBase {
-  using ConnPtr = Ref<PGconn>;
+  using ConnPtr = Ref<MYSQL>;
+  using ResPtr = Ref<MYSQL_RES>;
 
  public:
-  Iterator(const std::string& _sql, const ConnPtr& _conn);
-
-  Iterator(const Iterator& _other) = delete;
-
-  Iterator(Iterator&& _other) noexcept;
+  Iterator(const ResPtr& _res, const ConnPtr& _conn);
 
   ~Iterator();
 
   /// Whether the end of the available data has been reached.
-  bool end() const final;
+  bool end() const final { return end_; }
 
   /// Returns the next batch of rows.
   /// If _batch_size is greater than the number of rows left, returns all
@@ -36,24 +33,11 @@ class Iterator : public sqlgen::IteratorBase {
   Result<std::vector<std::vector<std::optional<std::string>>>> next(
       const size_t _batch_size) final;
 
-  Iterator& operator=(const Iterator& _other) = delete;
-
-  Iterator& operator=(Iterator&& _other) noexcept;
-
  private:
-  static std::string make_cursor_name() {
-    // TODO: Create unique cursor names.
-    return "sqlgen_cursor";
-  }
+  /// The underlying mysql result.
+  ResPtr res_;
 
-  /// Shuts the iterator down.
-  void shutdown();
-
- private:
-  /// A unique name to identify the cursor.
-  std::string cursor_name_;
-
-  /// The underlying postgres connection. We have this in here to prevent its
+  /// The underlying mysql connection. We have this in here to prevent its
   /// destruction for the lifetime of the iterator.
   ConnPtr conn_;
 
@@ -61,6 +45,6 @@ class Iterator : public sqlgen::IteratorBase {
   bool end_;
 };
 
-}  // namespace sqlgen::postgres
+}  // namespace sqlgen::mysql
 
 #endif
