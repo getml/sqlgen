ba4101dafc06d3a6565a8dff95072cf8caf052e3
Demian Nave <demian@thenaves.org>
2025-09-06 21:42:07 -0400

    Fix typos

--- tests/postgres/test_to_insert_or_replace_dry.cpp
+++ tests/postgres/test_to_insert_or_replace_dry.cpp
@@ -2,10 +2,10 @@
 
 #include <sqlgen.hpp>
 #include <sqlgen/dynamic/Insert.hpp>
-#include <sqlgen/postgres.hpp>
+#include <sqlgen/mysql.hpp>
 #include <sqlgen/transpilation/to_insert_or_write.hpp>
 
-namespace test_to_insert {
+namespace test_to_insert_or_replace_dry {
 
 struct TestTable {
   std::string field1;
@@ -15,7 +15,7 @@ struct TestTable {
   std::optional<std::string> nullable;
 };
 
-TEST(postgres, test_to_insert_or_replace) {
+TEST(mysql, test_to_insert_or_replace_dry) {
   static_assert(sqlgen::internal::has_constraint_v<TestTable>,
                 "The table must have a primary key or unique column for "
                 "insert_or_replace(...) to work.");
@@ -26,6 +26,6 @@ TEST(postgres, test_to_insert_or_replace) {
   const auto expected =
       R"(INSERT INTO "TestTable" ("field1", "field2", "field3", "id", "nullable") VALUES ($1, $2, $3, $4, $5) ON CONFLICT (field3, id) DO UPDATE SET field1=excluded.field1, field2=excluded.field2, field3=excluded.field3, id=excluded.id, nullable=excluded.nullable;)";
 
-  EXPECT_EQ(sqlgen::postgres::to_sql(insert_stmt), expected);
+  EXPECT_EQ(sqlgen::mysql::to_sql(insert_stmt), expected);
 }
-}  // namespace test_to_insert
+}  // namespace test_to_insert_or_replace_dry
