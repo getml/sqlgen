41b941265a8013e8a5ff7746254132d6b604bdc0
Dr. Patrick Urbanke <patrick@getml.com>
2025-09-16 23:18:11 +0200

    Added the DuckDBConnection

---
+++ include/sqlgen/duckdb/Connection.hpp
@@ -0,0 +1,89 @@
+#ifndef SQLGEN_DUCKDB_CONNECTION_HPP_
+#define SQLGEN_DUCKDB_CONNECTION_HPP_
+
+#include <duckdb.h>
+
+#include <memory>
+#include <rfl.hpp>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+#include "../IteratorBase.hpp"
+#include "../Ref.hpp"
+#include "../Result.hpp"
+#include "../Transaction.hpp"
+#include "../dynamic/Write.hpp"
+#include "../is_connection.hpp"
+#include "to_sql.hpp"
+
+namespace sqlgen::duckdb {
+
+class Connection {
+  using ConnPtr = Ref<duckdb3>;
+  using StmtPtr = std::shared_ptr<duckdb3_stmt>;
+
+ public:
+  Connection(const std::string& _fname)
+      : stmt_(nullptr), conn_(make_conn(_fname)) {}
+
+  static rfl::Result<Ref<Connection>> make(const std::string& _fname) noexcept;
+
+  ~Connection() = default;
+
+  Result<Nothing> begin_transaction() noexcept;
+
+  Result<Nothing> commit() noexcept;
+
+  Result<Nothing> execute(const std::string& _sql) noexcept;
+
+  Result<Nothing> insert(
+      const dynamic::Insert& _stmt,
+      const std::vector<std::vector<std::optional<std::string>>>&
+          _data) noexcept;
+
+  Result<Ref<IteratorBase>> read(const dynamic::SelectFrom& _query);
+
+  Result<Nothing> rollback() noexcept;
+
+  std::string to_sql(const dynamic::Statement& _stmt) noexcept {
+    return duckdb::to_sql_impl(_stmt);
+  }
+
+  Result<Nothing> start_write(const dynamic::Write& _stmt);
+
+  Result<Nothing> end_write();
+
+  Result<Nothing> write(
+      const std::vector<std::vector<std::optional<std::string>>>& _data);
+
+ private:
+  /// Generates the underlying connection.
+  static ConnPtr make_conn(const std::string& _fname);
+
+  /// Actually inserts data based on a prepared statement -
+  /// used by both .insert(...) and .write(...).
+  Result<Nothing> actual_insert(
+      const std::vector<std::vector<std::optional<std::string>>>& _data,
+      duckdb3_stmt* _stmt) const noexcept;
+
+  /// Generates a prepared statment, usually for inserts.
+  Result<StmtPtr> prepare_statement(const std::string& _sql) const noexcept;
+
+ private:
+  /// A prepared statement - needed for the read and write operations. Note that
+  /// we have declared it before conn_, meaning it will be destroyed first.
+  StmtPtr stmt_;
+
+  /// The underlying duckdb3 connection.
+  ConnPtr conn_;
+};
+
+static_assert(is_connection<Connection>,
+              "Must fulfill the is_connection concept.");
+static_assert(is_connection<Transaction<Connection>>,
+              "Must fulfill the is_connection concept.");
+
+}  // namespace sqlgen::duckdb
+
+#endif
