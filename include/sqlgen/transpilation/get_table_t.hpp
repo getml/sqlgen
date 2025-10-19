d06a6a41fa70ec307210846657d947781acace42
Dr. Patrick Urbanke <patrick@getml.com>
2025-10-19 17:18:06 +0200

    Add TableTupleType to GroupBy

--- include/sqlgen/transpilation/get_table_t.hpp
+++ include/sqlgen/transpilation/get_table_t.hpp
@@ -46,13 +46,13 @@ struct GetTableType<Literal<_alias>,
                 "Alias could not be identified.");
 };
 
-template <rfl::internal::StringLiteral _alias, class T>
-struct GetTableType<Literal<_alias>, TableWrapper<T>> {
+template <class T>
+struct GetTableType<Literal<"">, TableWrapper<T>> {
   using TableType = T;
 };
 
-template <rfl::internal::StringLiteral _alias, class T>
-struct GetTableType<Literal<_alias>, T> {
+template <class T>
+struct GetTableType<Literal<"">, T> {
   using TableType = T;
 };
 
