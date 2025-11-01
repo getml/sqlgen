ad8f23bea5a56e474cc9540d3f68c46a82fffac9
Dr. Patrick Urbanke <patrick@getml.com>
2025-11-01 13:21:18 +0100

    Improved the tests

--- tests/sqlite/test_cache.cpp
+++ tests/sqlite/test_cache.cpp
@@ -19,18 +19,27 @@ TEST(sqlite, test_cache) {
   const auto user = User{.name = "John", .age = 30};
   sqlgen::write(conn, user);
 
-  const auto query = sqlgen::read<User>;
+  using namespace sqlgen;
+  using namespace sqlgen::literals;
+
+  const auto query = sqlgen::read<User> | where("name"_c == "John");
 
   const auto cached_query = sqlgen::cache<100>(query);
-  const auto user1 = cached_query(conn).value();
+
+  const auto user1 = conn.and_then(cache<100>(query)).value();
+
+  EXPECT_EQ(cached_query.cache(conn).size(), 1);
+
   const auto user2 = cached_query(conn).value();
+  const auto user3 = cached_query(conn).value();
 
   EXPECT_EQ(user1.name, "John");
   EXPECT_EQ(user1.age, 30);
-  EXPECT_EQ(cached_query.cache(conn).size(), 1);
   EXPECT_EQ(user2.name, "John");
   EXPECT_EQ(user2.age, 30);
   EXPECT_EQ(cached_query.cache(conn).size(), 1);
+  EXPECT_EQ(user3.name, "John");
+  EXPECT_EQ(user3.age, 30);
 }
 
 }  // namespace test_cache
