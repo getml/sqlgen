ed9b40b6a77b4f9d67e2c0748cf53d36631782ef
Dr. Patrick Urbanke <patrick@getml.com>
2025-11-02 16:56:02 +0100

    SQLGEN_API to the beginning

--- include/sqlgen/internal/strings/strings.hpp
+++ include/sqlgen/internal/strings/strings.hpp
@@ -8,28 +8,28 @@
 
 namespace sqlgen::internal::strings {
 
-char SQLGEN_API to_lower(const char ch);
+SQLGEN_API char to_lower(const char ch);
 
-std::string SQLGEN_API to_lower(const std::string& _str);
+SQLGEN_API std::string to_lower(const std::string& _str);
 
-char SQLGEN_API to_upper(const char ch);
+SQLGEN_API char to_upper(const char ch);
 
-std::string SQLGEN_API to_upper(const std::string& _str);
+SQLGEN_API std::string to_upper(const std::string& _str);
 
-std::string SQLGEN_API join(const std::string& _delimiter,
+SQLGEN_API std::string join(const std::string& _delimiter,
                             const std::vector<std::string>& _strings);
 
-std::string SQLGEN_API replace_all(const std::string& _str,
+SQLGEN_API std::string replace_all(const std::string& _str,
                                    const std::string& _from,
                                    const std::string& _to);
 
-std::vector<std::string> SQLGEN_API split(const std::string& _str,
+SQLGEN_API std::vector<std::string> split(const std::string& _str,
                                           const std::string& _delimiter);
 
-std::string SQLGEN_API ltrim(const std::string& _str,
+SQLGEN_API std::string ltrim(const std::string& _str,
                              const std::string& _chars = " ");
 
-std::string SQLGEN_API rtrim(const std::string& _str,
+SQLGEN_API std::string rtrim(const std::string& _str,
                              const std::string& _chars = " ");
 
 inline std::string trim(const std::string& _str,
