e906980e4b0119d07d36898dc2e7fdf4f37b0aaa
Scott Fries <Scott@ScottFries.com>
2026-03-08 00:13:56 -0700

    Resolving uninitialized variables warnings

--- include/sqlgen/dynamic/types.hpp
+++ include/sqlgen/dynamic/types.hpp
@@ -23,90 +23,90 @@ struct Properties {
 
 // To be used as the default value.
 struct Unknown {
-  Properties properties;
+  Properties properties = Properties{};
 };
 
 struct Boolean {
-  Properties properties;
+  Properties properties = Properties{};
 };
 
 struct Dynamic {
   std::string type_name;
-  Properties properties;
+  Properties properties = Properties{};
 };
 
 struct Float32 {
-  Properties properties;
+  Properties properties = Properties{};
 };
 
 struct Float64 {
-  Properties properties;
+  Properties properties = Properties{};
 };
 
 struct Int8 {
-  Properties properties;
+  Properties properties = Properties{};
 };
 
 struct Int16 {
-  Properties properties;
+  Properties properties = Properties{};
 };
 
 struct Int32 {
-  Properties properties;
+  Properties properties = Properties{};
 };
 
 struct Int64 {
-  Properties properties;
+  Properties properties = Properties{};
 };
 
 struct JSON {
-  Properties properties;
+  Properties properties = Properties{};
 };
 
 struct UInt8 {
-  Properties properties;
+  Properties properties = Properties{};
 };
 
 struct UInt16 {
-  Properties properties;
+  Properties properties = Properties{};
 };
 
 struct UInt32 {
-  Properties properties;
+  Properties properties = Properties{};
 };
 
 struct UInt64 {
-  Properties properties;
+  Properties properties = Properties{};
 };
 
 struct Enum {
   std::string name;
   std::vector<std::string> values;
-  Properties properties;
+  Properties properties = Properties{};
 };
 
 struct Text {
-  Properties properties;
+  Properties properties = Properties{};
 };
 
 struct Date {
   std::string tz;
-  Properties properties;
+  Properties properties = Properties{};
 };
 
 struct Timestamp {
   std::string tz;
-  Properties properties;
+  Properties properties = Properties{};
 };
 
 struct TimestampWithTZ {
   std::string tz;
-  Properties properties;
+  Properties properties = Properties{};
 };
 
 struct VarChar {
   uint16_t length;
-  Properties properties;
+  Properties properties = Properties{};
 };
 
 }  // namespace sqlgen::dynamic::types
